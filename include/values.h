f4833f8c7263c844c18a6f7f155e968953110b1c
Lior Ykobovich <117036946+LiorYakoboich@users.noreply.github.com>
2023-01-07 15:34:19 +0200

    Valuse change

--- include/values.h
+++ include/values.h
@@ -41,18 +41,23 @@ const int WINDOW_WIDTH  = sf::VideoMode::getDesktopMode().width;
 //const int WINDOW_HEIGHT = 1800;
 //const int WINDOW_WIDTH  = 2000;
 const int MENU_CENTER    = WINDOW_WIDTH / 2;
-const int MENU_START_ROW = WINDOW_HEIGHT / 2.7;
-const float MENU_SCALE   = 0.0004* WINDOW_WIDTH;
+const int MENU_START_ROW = WINDOW_HEIGHT / 2.2;
+const float MENU_SCALE   = 0.0003* WINDOW_WIDTH;
 const int MENU_GAP       = MENU_SCALE * 200;
 const int MENU_PIC_WIDTH = 508;
 const int MENU_PIC_HEIGHT = 161;
+const int MENU_TEXT_SIZE = WINDOW_HEIGHT / 8.5;
+const int MENU_ROW = WINDOW_WIDTH / 4;
+const int MENU_COL = WINDOW_HEIGHT / 4.7;
+
+
 
 const int BOARD_WIDTH = 7*WINDOW_HEIGHT/8;
 const int MENU_WIDTH    = WINDOW_HEIGHT/15;
 const int START_ROW     = 150;
 const int START_COL     = 200;
-const int MENU_ROW      = WINDOW_HEIGHT/15;
-const int MENU_COL      = 3*WINDOW_WIDTH/4;
+//const int MENU_ROW      = WINDOW_HEIGHT/15;
+//const int MENU_COL      = 3*WINDOW_WIDTH/4;
 const int OBJECT        = 10;
 const int SPACE_SIZE    = 150;
 const int TILE_SIZE     = 2070;
